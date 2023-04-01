a2e89e1239695023ad3ef6c27ef4cc0e4f1deb43
MyHomie <1zemerovaanna@gmail.com>
2023-04-01 17:35:46 +0500

    first commit

---
+++ app/src/main/java/com/example/geoquiz/MainActivity3.kt
@@ -0,0 +1,31 @@
+package com.example.geoquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        trueButton = findViewById(R.id.buttontrue)
+        falseButton = findViewById(R.id.buttonfalse)
+        nextButton = findViewById(R.id.buttonnext)
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener{view: View ->
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
+        nextButton.setOnClickListener{view: View ->
+            val intent= Intent(this,MainActivity4::class.java)
+            startActivity(intent)
+        }
+    }
+}
